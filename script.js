11661f4ab2bfda345b7d36149f39f4a55df87c8d
Isabel-S <isabelsieh@gmail.com>
2023-08-20 19:23:53 -0700

    2 more sections

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 // using d3 for convenience
-var main = d3.select("main");
-var scrolly = main.select("#scrolly");
-var figure = scrolly.select("figure");
-var article = scrolly.select("article");
-var step = article.selectAll(".step");
+var main1 = d3.select("main");
+var scrolly1 = main1.select("#scrolly1");
+var figure1 = scrolly1.select("figure");
+var article1 = scrolly1.select("article");
+var step1 = article1.selectAll(".step");
+
 
 // initialize the scrollama
-var scroller = scrollama();
+var scroller1 = scrollama();
+
+// using d3 for convenience
+var main2 = d3.select("main");
+var scrolly2 = main2.select("#scrolly2");
+var figure2 = scrolly2.select("figure");
+var article2 = scrolly2.select("article");
+var step2 = article2.selectAll(".step");
+
+
+// initialize the scrollama
+var scroller2 = scrollama();
 
 // generic window resize listener event
-function handleResize() {
+function handleResize(step, figure, scroller) {
     // 1. update height of step elements
     var stepH = Math.floor(step.innerHeight * 0.5);
     var stepMargin = (window.innerHeight * 0.4);
@@ -30,12 +42,26 @@ function handleResize() {
 }
 
 // scrollama event handlers
-function handleStepEnter(response) {
+function handleStepEnter1(response) {
     console.log(response);
     // response = { element, direction, index }
 
     // add color to current step only
-    step.classed("is-active", function (d, i) {
+    step1.classed("is-active", function (d, i) {
+        return i === response.index;
+    });
+
+    // update graphic based on step
+    // figure.select("p").text(response.index + 1);
+}
+
+// scrollama event handlers
+function handleStepEnter2(response) {
+    console.log(response);
+    // response = { element, direction, index }
+
+    // add color to current step only
+    step2.classed("is-active", function (d, i) {
         return i === response.index;
     });
 
@@ -46,18 +72,29 @@ function handleStepEnter(response) {
 function init() {
 
     // 1. force a resize on load to ensure proper dimensions are sent to scrollama
-    handleResize();
+    handleResize(step1, figure1, scroller1);
 
     // 2. setup the scroller passing options
     // 		this will also initialize trigger observations
     // 3. bind scrollama event handlers (this can be chained like below)
-    scroller
+    scroller1
         .setup({
-            step: "#scrolly article .step",
+            step: "#scrolly1 article .step",
             offset: 0.7,
             debug: false
         })
-        .onStepEnter(handleStepEnter);
+        .onStepEnter(handleStepEnter1);
+
+    // 1. force a resize on load to ensure proper dimensions are sent to scrollama
+    handleResize(step2, figure2, scroller2);
+
+    scroller2
+        .setup({
+            step: "#scrolly2 article .step",
+            offset: 0.7,
+            debug: false
+        })
+        .onStepEnter(handleStepEnter2);
 }
 
 // kick things off
